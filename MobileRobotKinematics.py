41ffb7a9395759a72a65fc8b7e0c8b11a6108358
Ryan Barry <94549600+ryan-barry-99@users.noreply.github.com>
2024-04-18 22:20:05 -0400

    Update MobileRobotKinematics.py

--- MobileRobotKinematics.py
+++ MobileRobotKinematics.py
@@ -1,4 +1,4 @@
-'''
+"""
 This module provides the `MobileRobotKinematics` class which calculates the kinematic properties of any fixed-wheel mobile robot
 
 Default values for the `MobileRobotKinematics` class are defined at the top of the file. Changing these values to fit any robot
@@ -8,87 +8,115 @@ Requirements: numpy
 
 Author: Ryan Barry
 Date created: September 14, 2023
-'''
+"""
+
+from math import cos, pi, sin
 
 import numpy as np
-from math import cos, sin
-from Wheel import Wheel
+import rospkg
+import sys
+import rospy
+
+rospack = rospkg.RosPack()
+package_path = rospack.get_path('constants')
+sys.path.append(package_path + '/src')
+
+from RoverConstants import ALPHA, BASE_WIDTH, BETA, GAMMA, WHEEL_NAMES, WHEEL_RADIUS
 
 
-class MobileRobotKinematics :
-    def __init__(self):
+class MobileRobotKinematics:
+    def __init__(
+        self,
+        beta=BETA,
+        wheel_names=WHEEL_NAMES,
+        wheel_radius=WHEEL_RADIUS,
+        L=BASE_WIDTH,
+        alpha=ALPHA
+    ):
         """
         Initializes the `MobileRobotKinematics` instance.
-        
-        This class is used to calculate the kinematic properties of any fixed-wheel mobile robot.
+        :param beta: The angle between center of robot and wheel rotation axis for each wheel
+        :param wheel_names: The names of wheels in the robot.
+        :param wheel_radius: The radius of the wheel in meters.
+        :param L: The distance from the center of the robot to the center of the wheels in meters.
+        :param alpha: The angle offset from the robot's x-axis of each wheel
         """
-
         # Define variables
-        self.__wheels = []
-        self.__num_wheels = 0
-        self.update_jacobian()
-
-    def add_wheel(self, name, radius, L, beta=0, gamma=0, alpha=0):
-        wheel = Wheel(name, radius, L, beta, gamma, alpha)
-        self.__wheels.append(wheel)
-        self.__num_wheels += 1
-        self.update_jacobian()
-        return wheel
-    
-    def update_jacobian(self):
-        self.__r_theta = np.eye(self.__num_wheels)
+        self.__beta = beta
+        self.__wheel_radius = wheel_radius
+        self.__L = L
+        self.__wheel_names = wheel_names
+        self.__num_wheels = len(self.__wheel_names)
+        self.__alpha = alpha
+        self.__theta = 0 # Robot's angle relative to world frame
+        self.__r_theta = self.update_heading(self.__theta)
         self.__J1_list = []
         self.__C1_list = []
-        self.__J2 = np.eye(self.__num_wheels)
-        for i, wheel in enumerate(self.__wheels):
-            self.__J2[i,i] = wheel.radius
-            alpha = wheel.alpha
-            beta = wheel.beta
-            gamma = wheel.gamma
-            L = wheel.L
+        self.__J2 = np.eye(self.__num_wheels) * self.__wheel_radius
 
-            self.__J1_list.append(np.array([sin(alpha + beta + gamma), -cos(alpha + beta + gamma), -L*cos(beta + gamma)]))
-            self.__C1_list.append(np.array([cos(alpha + beta + gamma), sin(alpha + beta + gamma), L*sin(beta + gamma)]))
-            
-        self.__J1 = np.array(self.__J1_list).T
-        self.__C1 = np.array(self.__C1_list).T
-        self.__zeta_dot = np.zeros(3,1)
-        self.__phi = np.zeros(range(self.__num_wheels, 1))
+        for i, _ in enumerate(self.__wheel_names):
+            self.__J1_list.append(
+                np.array(
+                    [
+                        sin(self.__alpha[i] + self.__beta[i]),
+                        -cos(self.__alpha[i] + self.__beta[i]),
+                        -self.__L[i] * cos(self.__beta[i]),
+                    ]
+                )
+            )
+            self.__C1_list.append(
+                np.array(
+                    [
+                        cos(self.__alpha[i] + self.__beta[i]),
+                        sin(self.__alpha[i] + self.__beta[i]),
+                        self.__L[i] * sin(self.__beta[i]),
+                    ]
+                )
+            )
+        
+        self.__J1 = np.array(self.__J1_list)
+        self.__C1 = np.array(self.__C1_list)
+        self._zeta_dot = np.zeros((3, 1))
+        self._phi = np.zeros((self.__num_wheels, 1))
 
-    def calculate_robot_velocity(self, velocities):
-        self.__phi = np.array(velocities)
+    def calculate_robot_velocity(self, velocities: list):
+        self._phi = np.array(velocities)
         return self.forward_kinematics()
-    
-    def calculate_wheel_velocities(self, velocity):
-        self.__zeta_dot = np.array(velocity)
-        return self.inverse_kinematics()
 
+    def calculate_wheel_velocities(self, velocity: list):
+        self._zeta_dot = np.array(velocity)
+        return self.inverse_kinematics()
+    
+    def update_heading(self, theta):
+        self.__theta = theta
+        self.__r_theta = np.array(
+            [
+                [cos(self.__theta), sin(self.__theta), 0],
+                [-sin(self.__theta), cos(self.__theta), 0],
+                [0, 0, 1],
+            ]
+        )
 
     # Calculates the current linear and angular velocities of the robot
     def forward_kinematics(self):
-        '''
-        Calculates the vector of linear and angular velocities zeta_dot in the form 
+        """
+        Calculates the vector of linear and angular velocities zeta_dot in the form
                     [  x_dot  ] # linear velocity in the x direction
         zeta_dot =  [  y_dot  ] # linear velocity in the y direction
                     [ phi_dot ] # angular velocity about the z axis
-        '''
-        self.__zeta_dot = np.linalg.inv(self.__r_theta) @ self.__J1 @ self.__J2 @ self.__phi
-        return self.__zeta_dot
-  
+        """
+        self._zeta_dot = np.linalg.inv(self.__r_theta) @ np.linalg.pinv(self.__J1) @ self.__J2 @ self._phi
+        return self._zeta_dot
 
     # Calculates the inverse kinematics of the robot
     def inverse_kinematics(self):
-        '''
+        """
         Calculates the vector of wheel velocities necessary to achieve a targeted robot velocity
                 [ wheel_1 ]
         phi =   [ wheel_2 ]
                 [   ...   ]
                 [ wheel_n ]
-        '''
-        self.__phi = np.linalg.inv(self.__J2) @ self.__J1 @ self.__r_theta @ self.__zeta_dot
-        for i, wheel in enumerate(self.__wheels):
-            wheel.set_velocity(self.__phi[i])
-        return self.__phi
-    
-
-    
+        """
+        self.update_heading(self.__theta)
+        self._phi = np.linalg.pinv(self.__J2) @ self.__J1 @ self.__r_theta @ self._zeta_dot
+        return self._phi
